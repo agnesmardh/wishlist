6f20ad7ac69620f337f880151d778aee81067273
agnesmardh <agnes.mardh@gmail.com>
2018-11-11 11:59:09 +0100

    Use styled-components

--- src/components/AddListForm.js
+++ src/components/AddListForm.js
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 class AddListForm extends Component {
 
@@ -27,6 +38,7 @@ class AddListForm extends Component {
         <input type="text" className="form-control" placeholder="Wishlist name"
         value={this.state.newListName} onChange={this.handleChange}/>
         <div className="input-group-append">
+          <Button onClick={this.handleSubmit}>Add</Button>
           <button className="btn btn-outline-secondary" type="button" onClick={this.handleSubmit}>Add</button>
         </div>
       </div>
